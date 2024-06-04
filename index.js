1ef6c423cc1a858c70e836478e31ffee2c558ce4
lebomtomboti <lebomtomboti17@gmail.com>
2024-06-04 15:40:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//ageToDays//
+let age  = document.getElementById("ageInput");
+let age = 25
+function ageToDays(){
+    return age * 365
+}
+document.getElementById("result").innerHTML = "your age in days; " +days;
+console.log(ageToDays())
